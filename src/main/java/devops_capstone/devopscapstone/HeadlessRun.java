525519b64cdce648139a283f1221dae65c14a92e
brnster3210 <brnster3210@yahoo.com>
2019-06-19 16:52:44 -0700

    updated headless java app

--- src/main/java/devops_capstone/devopscapstone/HeadlessRun.java
+++ src/main/java/devops_capstone/devopscapstone/HeadlessRun.java
@@ -2,6 +2,7 @@ package devops_capstone.devopscapstone;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
 /**
@@ -13,7 +14,12 @@ public class HeadlessRun{
 		public static void main (String [] args) {
 			System.setProperty("webdriver.chrome.driver", "//Users//iBrown//Documents//DevOps//CaseStudy//chromedriver");
 			String expectedUrl = "http://54.175.170.133/";
-			WebDriver driver = new ChromeDriver();
+			
+			ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            options.addArguments("window-size=1200x600");
+			
+			WebDriver driver = new ChromeDriver(options);
 			driver.get(expectedUrl);
 			try{
 				Assert.assertEquals(expectedUrl, driver.getCurrentUrl());
